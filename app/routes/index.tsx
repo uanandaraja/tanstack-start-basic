b1187fcd92f53c17509435cf3254e2e81263c4d7
Unies Ananda Raja <uanandaraja@gmail.com>
2025-02-02 21:18:21 +0700

    make hero section a little better looking

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,34 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@headlessui/react";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: Home,
-})
+});
 
 function Home() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!!!</h3>
+    <div className="text-center space-y-8 pt-24">
+      <div className="inline-flex items-center gap-2 px-3 py-1 bg-white/10 rounded-full border mb-4">
+        <span className="text-xs font-semibold">STATUS: BETA</span>
+      </div>
+
+      <h1 className="text-5xl md:text-6xl lg:text-7xl font-semibold tracking-tight">
+        Full-stack React framework
+        <br />
+        powered by TanStack Router
+      </h1>
+
+      <p className="max-w-2xl mx-auto text-xl text-muted-foreground">
+        SSR, Streaming, Server Functions, API Routes, bundling and more powered
+        by TanStack Router, Nitro, and Vite. Ready to deploy to your favorite
+        hosting provider.
+      </p>
+
+      <div className="flex flex-wrap justify-center gap-4">
+        <Button className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white">
+          Get Started
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
