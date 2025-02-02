f5fbc23d0c431214e881b611eded93af49d57d1a
Unies Ananda Raja <uanandaraja@gmail.com>
2025-02-02 22:43:21 +0700

    add link to docs in hero

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -25,8 +25,14 @@ function Home() {
       </p>
 
       <div className="flex flex-wrap justify-center gap-4">
-        <Button className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white">
-          Get Started
+        <Button
+          as="a"
+          href="https://tanstack.com/router/latest/docs/framework/react/overview"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
+        >
+          Go to Docs
         </Button>
       </div>
     </div>
